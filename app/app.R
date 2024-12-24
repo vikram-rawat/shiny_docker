c7fd2612145e6675f98251c6cd1d551dca480b0b
Vikram Rawat <vickey.coool@gmail.com>
2024-12-24 16:56:26 +0530

    initialize

---
+++ app/app.R
@@ -0,0 +1,50 @@
+library(shiny)
+
+ui <- fluidPage(
+  titlePanel("Hello Shiny!"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput(
+        "obs",
+        "Number of Observations",
+        min = 10,
+        max = 1000,
+        value = 200
+      ),
+      radioButtons(
+        "action",
+        "Choose an Actions: ",
+        choices = c(
+          "Run" = "run",
+          "Crash" = "crash"
+        )
+      )
+    )
+  ),
+  mainPanel(
+    plotOutput("distPlot")
+  )
+)
+
+server <- function(input, output, session) {
+  observeEvent(
+    input$action,
+    {
+      if (input$action == "crash") {
+        stopApp("App Crashed as Requested")
+      }
+    }
+  )
+
+  output$distPlot <- renderPlot({
+    hist(rnorm(input$obs))
+  })
+}
+
+shinyApp(
+  ui = ui,
+  server = server,
+  options = list(
+    host = "0.0.0.0"
+  )
+)
